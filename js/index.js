c6fa8a178597b6b841efbec340eb44d511fb319f
Dodger-42 <kevin.bing.barber@gmail.com>
2020-06-05 18:14:12 -0700

    first

--- js/index.js
+++ js/index.js
@@ -1,6 +1,20 @@
 // Your code goes here
 const adventureImg = document.querySelector('.img-content img');
 adventureImg.addEventListener('mouseenter',() => {
-console.log('Hello world');
+// console.log('Hello world');
+});
+
+const busImg = document.querySelector('.intro img');
+busImg.addEventListener('mouseenter',() => {
+console.log('What up');
+
+    busImg.style.transform = "scale(1.2)";
+    busImg.style.transition = "transform 1s";
+});
+
+
+busImg.addEventListener('mouseleave', () => {
+    busImg.style.transform = 'scale(1)';
+    busImg.style.transition = "transform 1s"
 });
 
