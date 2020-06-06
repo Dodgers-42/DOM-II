9b2f40cc2eac3b7c6fddcaa76441ad09d9e44d94
Dodger-42 <kevin.bing.barber@gmail.com>
2020-06-05 19:08:16 -0700

    first

--- js/index.js
+++ js/index.js
@@ -1,20 +1,49 @@
 // Your code goes here
+
+// mouseenter
+
 const adventureImg = document.querySelector('.img-content img');
 adventureImg.addEventListener('mouseenter',() => {
 // console.log('Hello world');
 });
 
+// mouseenter w/ transfor and transition
+
 const busImg = document.querySelector('.intro img');
 busImg.addEventListener('mouseenter',() => {
-console.log('What up');
+// console.log('What up');
 
     busImg.style.transform = "scale(1.2)";
     busImg.style.transition = "transform 1s";
 });
 
+// mouseleave
 
 busImg.addEventListener('mouseleave', () => {
     busImg.style.transform = 'scale(1)';
     busImg.style.transition = "transform 1s"
+});
+
+// click
+
+document.querySelectorAll('.nav-link').forEach((a) => {
+    a.addEventListener('click', (e) => {
+        // console.log(e);
+        a.style.color = 'dodgerblue';
+    });
+});
+
+// dblclick event
+
+const contentImg = document.querySelector('.content-destination img');
+contentImg.addEventListener('dblclick', () => {
+    contentImg.src = "img/fun-bus.jpg";
+});
+
+// resize 
+
+const vanPic = document.querySelector('.img-content img');
+window.addEventListener('resize', () => {
+    vanPic.src = "https://images.fineartamerica.com/images/artworkimages/mediumlarge/3/starry-night-by-vincent-van-gogh-vincent-van-gogh.jpg";
 });
 
